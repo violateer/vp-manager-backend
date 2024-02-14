52ca699ce3a6bfd6ea2de3c283a7245367242b58
root <root@192.168.2.21>
2024-02-14 16:59:31 +0800

    取消项目名称不重复校验

--- app/controllers/api/v1/registers_controller.rb
+++ app/controllers/api/v1/registers_controller.rb
@@ -4,15 +4,18 @@ class Api::V1::RegistersController < ApplicationController
 
     user = User.select(:id, :email, :name, :phone, :avatar, :created_at, :updated_at).find_by_email(params[:email])
     if user.nil?
-      newUser = User.new email: params[:email], password: params[:password], name: params[:name]
+      newUser = User.new email: params[:email], password: params[:password], name: params[:name], phone: params[:phone]
       if newUser.save
           # 创建账号时默认创建一个自己的项目
           project = newUser.projects.create name: "我的个人空间", manager_user: newUser
-          project.save
 
-          newUser.update active_project: project
+          if project.save
+            newUser.update active_project: project
 
-          render json: { token: newUser.generate_jwt }, status: :ok
+            render json: { token: newUser.generate_jwt }, status: :ok
+          else
+              render json: { errors: project.errors }, status: :unprocessable_entity
+          end
       else
           render json: { errors: newUser.errors }, status: :unprocessable_entity
       end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :manager_user, class_name: "User", optional: true
 
-  validates :name, presence: { message: "项目名称不能为空！" }, uniqueness: { message: "项目名称已存在！" }
+  validates :name, presence: { message: "项目名称不能为空！" }
 end
