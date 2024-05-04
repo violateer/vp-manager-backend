05f4810e250b6b6029534c4b20ca93474433297d
violateer <1828257089@qq.com>
2024-05-04 19:55:49 +0800

    projects/tree

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,22 +4,18 @@ class Project < ApplicationRecord
 
   validates :name, presence: { message: "项目名称不能为空！" }
   
-  def self.tree_structure(projects)
-    project_hash = projects.index_by(&:id)
-    tree = []
-
-    projects.each do |project|
-      parent_id = project.parent_id 
-
-      if parent_id.nil?
-        tree << project.attributes.merge(children: [])
-      else
-        parent = project_hash[parent_id]
-        parent.children ||= []
-        parent.children << project.attributes.merge(children: [])
-      end
+  def self.tree_structure(projects_base, parent_id = nil)
+    projects = projects_base.where(parent_id: parent_id)
+  
+    # 递归构建树结构
+    tree = projects.map do |project|
+      project_data = project.attributes.symbolize_keys
+      children = tree_structure(projects_base, project.id)
+      project_data[:children] = children if children.present?
+      project_data
     end
-
+  
+    # 返回树形结构
     tree
   end
 end
